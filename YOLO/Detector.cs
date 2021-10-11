c708c5f56509d8a06a037ee1b2daaa68b1c1e37c
panfach <Andept@yandex.ru>
2021-10-11 14:00:33 +0300

    Fixed: Added the second project, fixed code

--- YOLO/Detector.cs
+++ YOLO/Detector.cs
@@ -15,16 +15,9 @@ namespace YOLO
     {
         static readonly string[] classesNames = new string[] { "person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush" };
         static readonly string modelPath = @"D:\And\Prog\onnx\models-master\vision\object_detection_segmentation\yolov4\model\yolov4.onnx";
-        static BufferBlock<IReadOnlyList<YoloResult>> outputBuffer = new();
-        static BufferBlock<float> progress = new();
-        public static List<IReadOnlyList<YoloResult>> output = new();
-
-        public static int ImageAmount;
-        public static BufferBlock<float> Progress => progress;
-        public static List<IReadOnlyList<YoloResult>> Output => output;
 
 
-        public static async void ExecuteAsync(string directoryPath, CancellationToken token)
+        public static async Task ExecuteAsync(string directoryPath, CancellationToken token, BufferBlock<IReadOnlyList<YoloResult>> output)
         {
             MLContext mlContext = new MLContext();
 
@@ -52,54 +45,46 @@ namespace YOLO
 
             var model = pipeline.Fit(mlContext.Data.LoadFromEnumerable(new List<YoloBitmapData>()));
 
-            string[] filenames = Directory.GetFiles(directoryPath, "*.jpg");
-            ImageAmount = filenames.Length;
-            Console.WriteLine(ImageAmount + " images were found.");
+            string[] filenames = GetImageFilenames(directoryPath);
 
-            BufferBlock<int> processingImageBuffer = new();
-            Parallel.For(0, filenames.Length, i => processingImageBuffer.Post(i));
-            processingImageBuffer.Complete();
-
-            ActionBlock<int> processingImageActionBlock = new ActionBlock<int>(
-                async x =>
+            var processingImageActionBlock = new ActionBlock<string>(
+                async filename =>
                 {
                     //Console.WriteLine("ActionBlock Start");
                     if (token.IsCancellationRequested) return;
-                    int filenameIndex = await processingImageBuffer.ReceiveAsync();
                     await Task.Run(() =>
                     {
                         //Console.WriteLine("Processing...");
-                        using (var bitmap = new Bitmap(Image.FromFile(filenames[filenameIndex])))
+                        using (var bitmap = new Bitmap(Image.FromFile(filename)))
                         {
                             if (token.IsCancellationRequested) return;
                             var predictionEngine = mlContext.Model.CreatePredictionEngine<YoloBitmapData, YoloPrediction>(model);
                             var predict = predictionEngine.Predict(new YoloBitmapData() { Image = bitmap });
                             if (token.IsCancellationRequested) return;
                             var results = predict.GetResults(classesNames, 0.3f, 0.7f);
-                            outputBuffer.Post(results);
-                            progress.Post((float)outputBuffer.Count / ImageAmount);
+
+                            output.Post(results);
                         }
                     });
                     //Console.WriteLine("ActionBlock End");
                 },
                 new ExecutionDataflowBlockOptions
                 {
-                    MaxDegreeOfParallelism = 8
+                    MaxDegreeOfParallelism = Environment.ProcessorCount
                 }
             );
 
-            Parallel.For(0, filenames.Length, i => processingImageActionBlock.Post(i));
+            foreach (string item in filenames)
+                processingImageActionBlock.Post(item);
+
             processingImageActionBlock.Complete();
             await processingImageActionBlock.Completion;
+            output.Complete();
+        }
 
-            progress.Complete();
-            outputBuffer.Complete();
-
-            output.Clear();
-            while (outputBuffer.Count > 0)
-            {
-                output.Add(outputBuffer.Receive());
-            }
+        public static string[] GetImageFilenames(string directoryPath)
+        {
+            return Directory.GetFiles(directoryPath, "*.jpg");
         }
     }
 }
