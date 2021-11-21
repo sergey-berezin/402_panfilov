1e4b1f0cc65f915dca3869ecbf0ab41708fb8d8d
panfach <Andept@yandex.ru>
2021-11-21 12:18:40 +0300

    Lab 3 | 21.11.2021

---
+++ DataBase/DataBaseManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class DataBaseManager
+    {
+        private LibraryContext db = new();
+
+        public event Action DataChanged;
+
+
+        public bool Add(Item item)
+        {
+            if (CheckItemPresence(item)) return false;
+
+            db.Items.Add(item);
+            db.SaveChanges();
+            DataChanged?.Invoke();
+            return true;
+        }
+
+        public async Task AddAsync(Item item)
+        {
+            if (CheckItemPresence(item)) return;
+
+            await db.Items.AddAsync(item);
+            await db.SaveChangesAsync();
+
+            await Task.Delay(1);           // Без этой строки ui зависает
+
+            DataChanged?.Invoke();
+        }
+
+        public void Clear()
+        {
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            DataChanged?.Invoke();
+        }
+
+        bool CheckItemPresence(Item item)
+        {
+            bool result = false;
+            var query = db.Items.Where(x => (x.X1 == item.X1) && (x.X2 == item.X2) && (x.Y1 == item.Y1) && (x.Y2 == item.Y2));
+            foreach (var elem in query)
+            {
+                if (elem.Image.SequenceEqual(item.Image))
+                    result = true;
+            }
+
+            return result;
+        }
+
+        public IEnumerable<string> GetClassList()
+        {
+            IEnumerable<string> result;
+            result = db.Items.Select(x => x.Label).Distinct().Select(x => $"[{db.Items.Where(y => y.Label == x).Count()}] {x}");
+            return result;
+        }
+
+        public IEnumerable<byte[]> GetImageList(string label)
+        {
+            IEnumerable<byte[]> result;
+            result = db.Items.Where(x => x.Label == label).Select(x => x.Image);
+            return result;
+        }
+    }
+}
---
+++ DataBase/LibraryContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataBase
+{
+    public partial class Item
+    {
+        public int ItemId { get; set; }
+
+        public float X1 { get; set; }
+        public float X2 { get; set; }
+        public float Y1 { get; set; }
+        public float Y2 { get; set; }
+        public byte[] Image { get; set; }
+        public string Label { get; set; }
+        public float Confidence { get; set; }
+    }
+
+    class LibraryContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        public LibraryContext()
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder b)
+        {
+            b.Entity<Item>().HasKey(x => x.ItemId);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder b) => b.UseSqlite(@"Data Source=D:\And\Prog\cs\402_panfilov\DataBase\library.db");
+    }
+}
